21ac678bf3bd9ccd0912bf284728391650bb8341
fetgrigory <97grfetkulin@mail.ru>
2026-06-19 20:05:36 +0300

    feat(reviews): add review model and initial app configuration

--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from apps.users.models import User
+from apps.rooms.models import Room
 
-# Create your models here.
+
+# Reviews table
+class Review(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    room = models.ForeignKey(
+        Room,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    # Review details and sentiment analysis results
+    review_text = models.TextField()
+    sentiment_label = models.CharField(max_length=255)
+    sentiment_score = models.FloatField()
+    date = models.DateField(default=timezone.localdate)
+
+    # String representation for admin panel
+    def __str__(self):
+        return f"Отзыв от пользователя {self.user} для номера {self.room}"
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
