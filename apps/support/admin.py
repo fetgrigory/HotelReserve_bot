b17135183920f2ea0995ed65070e36bb08f41fec
fetgrigory <97grfetkulin@mail.ru>
2026-06-29 22:46:43 +0300

    chore(support): add FAQ to admin interface display

--- apps/support/admin.py
+++ apps/support/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import FAQ
 
-# Register your models here.
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ("question", "is_active", "created_at")
+    search_fields = ("question", "answer")
+    list_filter = ("is_active",)
+    fields = ("question", "answer", "is_active")
